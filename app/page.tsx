7b4059860efe3b454b2c1c336df838c742a62c04
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2025-05-24 08:47:49 -0500

    added gregmox

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ export default function Home() {
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="flex flex-col justify-center gap-4 mt-10 sm:max-w-[400px] mx-auto">
       <DashboardButton label="Band Schedule" url="https://docs.google.com/spreadsheets/u/0/d/1naOVRBDOi6G_Amtr8U06ITkPpGE9vQC9/htmlview#" isExternal={true} />
+      <DashboardButton label="GregMox" url="https://gregmox.c-syncapp.com" isExternal={true} />
       <DashboardButton label="PiHole" url="http://10.24.24.25/admin/login" isExternal={true} />
       </div>
     </section>
