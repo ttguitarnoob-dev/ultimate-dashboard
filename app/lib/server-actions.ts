86bcd7c0aa03f39e28188757bfee66dc6c2367a4
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2025-07-29 22:22:41 +0000

    added prisma integration - also added a route for carry item

---
+++ app/lib/server-actions.ts
@@ -0,0 +1,24 @@
+
+
+//GET CARRY ITEMS
+
+import { prisma } from "./db";
+
+export async function GetCarryItemsI() {
+
+    try {
+        const items = await prisma.carryItem.findMany({
+          orderBy: {
+            createdAt: 'desc', // Optional: sort newest first
+          },
+        });
+        return items;
+      } catch (error) {
+        console.error('Error retrieving CarryItems:', error);
+    
+        // Optionally, rethrow a custom error if you're using error boundaries or want better control
+        throw new Error('Failed to retrieve carry items from the database.');
+      } finally {
+        await prisma.$disconnect();
+      }
+}
