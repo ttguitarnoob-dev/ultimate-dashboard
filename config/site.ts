275e44990ffdbbaed4e85e2126dc7e306b6fcf79
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2025-07-30 15:18:01 +0000

    made icon and changed site info

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "KittyCottage Dashboard",
+  description: "Center for all the Kitty Cottage stuff",
   navItems: [
     {
       label: "Home",
